f81563c5547fc58a4db41e44315934bd39d8cca9
rioles <rioles1992@gmail.com>
2022-03-11 01:30:54 +0000

    variable size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
     	float floatType;
     	char charType;
 	double doubleType;
-	printf("Size of a char:%zu byte(s)\n", sizeof(charType));
-	printf("Size of an int:%zu byte(s)\n", sizeof(intType));
-        printf("Size of a long int:%li byte(s)\n", sizeof(long));
-        printf("Size of a long long int:%ld byte(s)\n", sizeof(doubleType));
-        printf("Size of a float:%zu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+        printf("Size of a long int :%li byte(s)\n", sizeof(long));
+        printf("Size of a long long int: %ld byte(s)\n", sizeof(doubleType));
+        printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
