05451b243cd269fafe2df9ce865e550343e2c60f
rioles <rioles1992@gmail.com>
2022-03-12 16:10:54 +0000

    printe number from 0 to 9 with comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 10; x <= 19; x++)
+	{
+		putchar(x % 10 + '0');
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+	return (0);
+}
