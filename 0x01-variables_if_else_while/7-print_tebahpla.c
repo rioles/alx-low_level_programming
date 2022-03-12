5bd1362c3ceeffea9777f59145c237ec18815b0c
rioles <rioles1992@gmail.com>
2022-03-12 15:45:24 +0000

    print reverse a to z

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar(10);
+	return (0);
+}
