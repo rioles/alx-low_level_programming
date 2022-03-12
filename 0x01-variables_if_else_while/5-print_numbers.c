4ffd53517ebe98abbf5160904ef0816612f685df
rioles <rioles1992@gmail.com>
2022-03-12 13:52:30 +0000

    print number from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	putchar(10);
+	return (0);
+}
