9c818f263bcfd40f2e98a0dc0b702b8b9a184ccf
rioles <rioles1992@gmail.com>
2022-03-12 12:36:12 +0000

    print alphabet from a to z with putchar)

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	for (int alphabetNumeric = 'a'; a <= 'z'; alphabetNumeric++)
+	{
+		putchar(alphabetNumeric);
+	}
+	printf("\n");
+	return (0);
+}
