af4fe4795d9ffacd6f80af71d07d6f67ca8b38f0
rioles <rioles1992@gmail.com>
2022-03-15 23:38:05 +0000

    sum off two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * add - Return sum of two numbers
+ * and check the number's sign
+ *
+ *@a: first value
+ *@b: second value
+ * Return: 1 if lowercase 0 otherwise(Success)
+ */
+int add(int a, int b)
+{
+	reuslt = a + b;
+	return (result);
+}
