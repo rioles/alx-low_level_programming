85be4ba0408c4bccd2829993be987e898909d1aa
rioles <rioles1992@gmail.com>
2022-03-15 20:18:27 +0000

    check if if character is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_alphabet - Print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+ int _islower(int value)
+ {
+ 	if (islower(value))
+ 	{
+ 		return 1;
+ 	}
+ 	else
+ 	{
+ 		return 0;
+
+ 	}
+ }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 void putChar(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 #endif
