cdee65ce6f159d1cef7094b5fb52d49124df0a3f
rioles <rioles1992@gmail.com>
2022-03-15 22:21:41 +0000

    abs number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * print_sign - Check if number is less or gretter than 0
+ * and check the number's sign
+ *
+ * @n: The number to check
+ *
+ * Return: 1 if lowercase 0 otherwise(Success)
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		n * = -1;
+		return (n);
+	}
+	else if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (0);
+	}
+}
