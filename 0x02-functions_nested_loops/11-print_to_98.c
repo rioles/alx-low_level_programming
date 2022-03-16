7826fe1356a3c940868f1305174f4b40edd7b3ce
rioles <rioles1992@gmail.com>
2022-03-16 00:15:06 +0000

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * print_to_98 - Print number ended to 98
+ * and check the number's sign
+ *
+ *@n: number
+ * Return: 1 if lowercase 0 otherwise(Success)
+ */
+int  print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n ; n <= 98; n++)
+		{
+
+			printf("%d", n);
+			if (n == 98)
+			{
+				continue;
+			}
+			printf(",");
+			printf(" ");
+		}
+
+		printf("\n");
+	}
+	else if (n >= 98)
+	{
+		for (n = n ; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n == 98)
+			{
+				continue;
+			}
+			printf(",");
+			printf(" ");
+		}
+		printf("\n");
+	}
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,4 +11,5 @@ int _abs(int);
 int print_last_digit(int n);
 void jack_bauer(void);
 int add(int, int);
+void print_to_98(int n);
 #endif
