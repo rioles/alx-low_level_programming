02d103849f7270e1e12d7f4c11050ad71ce73c67
rioles <rioles1992@gmail.com>
2022-03-17 13:20:28 +0000

    multiplie two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * mul - multiplie two integers
+ *
+ *@a: first integer
+ *@b: second integer
+ *Return: return a multiplie by b
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define main_h
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
