b9d300a83129e4646c91d00c56a3a1c889f1d557
rioles <rioles1992@gmail.com>
2022-03-17 14:22:47 +0000

    printing number from 0 to 14 ten time

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_number_from_zero_to_forteen - print number from 0 to 14
+ *
+ */
+
+void print_number_from_zero_to_forteen(void)
+{
+	int number;
+
+	for (number = 0; number < 15; number++)
+	{
+		printf("%d", number);
+	}
+	putchar(10);
+}
+/**
+ *more_numbers - print number from 0 to 14 ten time
+ */
+void more_numbers(void)
+{
+	int time_to_print_number;
+
+	for (time_to_print_number = 0; time_to_print_number < 11;
+			time_to_print_number++)
+	{
+		print_number_from_zero_to_forteen();
+	}
+}
