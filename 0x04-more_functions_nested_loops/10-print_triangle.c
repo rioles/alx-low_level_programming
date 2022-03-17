09b626392fb31fd062b2f17b98949049c25b5681
rioles <rioles1992@gmail.com>
2022-03-17 23:17:58 +0000

    draw tiangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,13 +13,12 @@ void print_triangle(int n)
 	for (i = 0; i < n; i++)
 	{
 		int j;
+		int k;
 
 		for (j = 1; j < n - i; j++)
 		{
 			printf(" ");
 		}
-		int k;
-
 		for (k = 0; k <= 2 * i - i; k++)
 		{
 			printf("#");
