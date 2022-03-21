4b932e8c7264c678fe74d27e6ba56bc24227e959
rioles <rioles1992@gmail.com>
2022-03-21 21:42:42 +0000

    return size of array of char type

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ *_strlen - return size os array's String
+ *
+ *@s: array of typ char
+ *Return: size of array of char type
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
