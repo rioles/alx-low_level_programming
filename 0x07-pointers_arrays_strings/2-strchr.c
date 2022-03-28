a455e5a421556525b9e55d9c1858efe1d84c7bdd
rioles <rioles1992@gmail.com>
2022-03-28 16:22:53 +0000

    correction

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; *(s + i) != '\0'; i++)
+	while (*s)
 	{
-		if (*(s + i) == c)
-		{
-			return (s + i);
-		}
+		if (*s != c)
+			s++;
+		else
+			return (s);
 	}
+	if (c == '\0')
+		return (s);
+
 	return (NULL);
 }
