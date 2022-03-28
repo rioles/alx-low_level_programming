af4cee92b81170b3c805c86e2dc3fdf02a4eea0b
rioles <rioles1992@gmail.com>
2022-03-28 16:03:24 +0000

    function that locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strchr -  function that locates a character in a string.
+ *
+ * @s: the string of characters that a character wants to be localized
+ * @c: character that will be localized
+ * Return: return c
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i < *(s + i) = '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
