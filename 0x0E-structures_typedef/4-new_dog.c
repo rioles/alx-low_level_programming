3ca15b62d5aee893e4de36f4d6b8b5b00e27ca67
rioles <rioles1992@gmail.com>
2022-04-11 16:39:07 +0000

    a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,30 +11,13 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int nlen, olen, i;
-	dog_t *doggy;
+	struct dog *d;
 
-	nlen = olen = 0;
-	while (name[nlen++])
-		;
-	while (owner[olen++])
-		;
-	doggy = malloc(sizeof(dog_t));
-	if (doggy == NULL)
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
 		return (NULL);
-
-	doggy->name = malloc(nlen * sizeof(doggy->name) + 1);
-	if (doggy == NULL)
-		return (NULL);
-	for (i = 0; i < nlen; i++)
-		doggy->name[i] = name[i];
-
-	doggy->age = age;
-
-	doggy->owner = malloc(olen * sizeof(doggy->owner) + 1);
-	if (doggy == NULL)
-		return (NULL);
-	for (i = 0; i < olen; i++)
-		doggy->owner[i] = owner[i];
-	return (doggy);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
 }
