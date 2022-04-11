067a959684b046428889c4c9e8dc3f3ff77114fb
rioles <rioles1992@gmail.com>
2022-04-11 15:10:13 +0000

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Dog definition
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: Dog Definition
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
